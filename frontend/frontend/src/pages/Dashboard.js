475b3c7eb698f7ccaa762e16197954af9d24ceef
n_sam <n_sammacheep@outlook.com>
2023-10-21 17:51:30 -0400

    Barely made a box on left side

--- frontend/frontend/src/pages/Dashboard.js
+++ frontend/frontend/src/pages/Dashboard.js
@@ -3,35 +3,12 @@ import app from "../axiosConfig.js";
 import { useState } from "react";
 
 export default function Dashboard() {
-  const [testVar, setTestVar] = useState('')
-  const getVar = () => {
-    app.get('/example').then((response) => {
-      setTestVar(response.data.msg)
-    })
-  }
-  getVar()
 
     return (
 
-      <div className="login">
-      <header className="login-header">
-        <h1 className="text-3xl font-bold underline">
-          TESTING TAILWIND CSS
-        </h1>
-        <a
-          className="login-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          testing changing something ;  to get something from backend: {testVar}
-
-        </p>
-      </header>
-      </div>
+    <div className="h-screen bg-gradient-to-b from-blue-200 to-blue-500">
+      <div class="box-border h-screen w- p-4 border-4 "></div>
+    </div>
     );
 }
 
