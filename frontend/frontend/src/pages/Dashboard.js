129c8d179760c43333782d9436821784d19a5a47
n_sam <n_sammacheep@outlook.com>
2023-10-21 19:55:04 -0400

    Pop up alert and gradient background

--- frontend/frontend/src/pages/Dashboard.js
+++ frontend/frontend/src/pages/Dashboard.js
@@ -11,28 +11,40 @@ import Tadpole from "../imgs/Tadpole.png"
 
 export default function Dashboard() {
 
-    return (
+  const logMedicine = () => {
+    let text = "Testing";
+    if (window.confirm(text) == true) {
+      
+    }
+    else {
 
-      <div className="">
-        <div className="flex-row text-center">
-          <h1 className="text-3xl font-bold underline">
-            TESTING TAILWIND CSS
-          </h1>
-        </div>
-        <div className="flex">
-          <div className="COLUMN 1 w-1/3 flex flex-col">
-            this is where the sticky note thing will be
+    }
+  };
+
+    return (
+      <div class="h-screen bg-gradient-to-b 
+      from-blue-200 to-blue-500">
+        <div className="">
+          <div className="flex-row text-center">
+            <h1 className="text-3xl font-bold underline">
+              TESTING TAILWIND CSS
+            </h1>
           </div>
-          <div className="w-2/3 flex flex-col">
-              <img src={Lily_Pad}></img>
+          <div className="flex">
+            <div className="COLUMN 1 w-1/3 flex flex-col">
+              this is where the sticky note thing will be
+            </div>
+            <div className="w-2/3 flex flex-col">
+                <img src={Lily_Pad}></img>
+            </div>
           </div>
-        </div>
-        <div className="flex text-center">
-          <div className="COLUMN 1 w-1/3 flex flex-col">
-            <button>This is a button to add a new prescription</button>
-          </div>
-          <div className="w-2/3 flex flex-col">
-            <button>This is a button to log medicine</button>
+          <div className="flex text-center">
+            <div className="COLUMN 1 w-1/3 flex flex-col">
+              <button>This is a button to add a new prescription</button>
+            </div>
+            <div className="w-2/3 flex flex-col">
+              <button onClick={logMedicine}>This is a button to log medicine</button>
+            </div>
           </div>
         </div>
       </div>
@@ -40,6 +52,3 @@ export default function Dashboard() {
 
 
 }
-
-
-
