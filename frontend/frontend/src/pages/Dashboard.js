4b02dbae1610f9b57db79c7f4e1ac7f1d92eafd9
Gabe Hoberman <ghoberman@ufl.edu>
2023-10-21 21:05:30 -0400

    gabe commit

--- frontend/frontend/src/pages/Dashboard.js
+++ frontend/frontend/src/pages/Dashboard.js
@@ -11,36 +11,42 @@ import Pill from "../imgs/Pill.png"
 
 export default function Dashboard() {
 
+  const logMedicine = () => {
+    let text = "Testing";
+    if (window.confirm(text) == true) {
+      
+    }
+    else {
+
+    }
+  };
+
     return (
-
-      <div className="login">
-      <header className="login-header">
-        <h1 className="text-3xl font-bold underline">
-          TESTING TAILWIND CSS
-        </h1>
-        <div class="absolute bottom-20 left-20">
-          <img src = {Lily_Pad}>
-          </img>
+      <div class="h-screen bg-gradient-to-b 
+      from-blue-200 to-blue-500">
+        <div className="">
+          <div className="flex-row text-center">
+            <h1 className="text-3xl font-bold underline">
+              TESTING TAILWIND CSS
+            </h1>
+          </div>
+          <div className="flex">
+            <div className="COLUMN 1 w-1/3 flex flex-col">
+              this is where the sticky note thing will be
+            </div>
+            <div className="w-2/3 flex flex-col">
+                <img src={Lily_Pad}></img>
+            </div>
+          </div>
+          <div className="flex text-center">
+            <div className="COLUMN 1 w-1/3 flex flex-col">
+              <button>add medicine</button>
+            </div>
+            <div className="w-2/3 flex flex-col">
+              <button onClick={logMedicine}>This is a button to log medicine</button>
+            </div>
+          </div>
         </div>
-          
-        <a
-          className="login-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          testing changing something ;  to get something from backend:
-
-        </p>
-      </header>
       </div>
     );
-
-
 }
-
-
-
